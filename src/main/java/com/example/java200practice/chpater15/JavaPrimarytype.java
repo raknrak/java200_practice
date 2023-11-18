21e4c86a6764cac5f8e4a5d6ac2229bdcf3ec4f5
Gyeongrak Choi <peter6617@gmail.com>
2023-11-18 23:15:25 +0900

    참조 타입 사용하기 연습

--- src/main/java/com/example/java200practice/chpater15/JavaPrimarytype.java
+++ src/main/java/com/example/java200practice/chpater15/JavaPrimarytype.java
@@ -8,7 +8,7 @@ public class JavaPrimarytype {
         char suit = 'H';    //'H'를 문자 타입 변수 suit에 저장한다.
         int year = 2016;    // 소수점 없는 정수를 year에 저장한다.
         // 소수점(.)이 있는 실수를
-        double latitude = 37.52127220511242; // latitude와 
+        double latitude = 37.52127220511242; // latitude와
         double longitude = 127.0074462890625; // longitude에 각각 저장한다.
         // "\t"는 이스케이프 문자로 탭(7자 띄우기)을 의미한다.
         System.out.println(latitude + "\t" + longitude);
