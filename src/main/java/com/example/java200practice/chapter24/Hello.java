3ceac074630575b5da67b2d5f9efef11ccc281f7
Gyeongrak Choi <peter6617@gmail.com>
2023-11-21 22:32:09 +0900

    메소드 선언하고 호출하기, 반환 타입이 있는 메소드와 없는 메소드

---
+++ src/main/java/com/example/java200practice/chapter24/Hello.java
@@ -0,0 +1,20 @@
+package com.example.java200practice.chapter24;
+
+public class Hello {
+    //메소드 선언
+    public static void showHello() {
+        System.out.println("-------------------------------------");
+        System.out.println(" 이 프로그래밍은 Java200이 만들었습니다.");
+        System.out.println("-------------------------------------");
+    }
+
+    //메소드 호출
+    public static void main(String[] args) {
+        showHello(); // 메소드 이름
+    }
+}
+/*      -------------------------------------
+        이 프로그래밍은 Java200이 만들었습니다.
+        -------------------------------------*/
+
+
